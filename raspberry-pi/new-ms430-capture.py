62e74775085aec257bde3fb4776e5e5f9773e475
Wesley Archer <wesleyarcher@hotmail.com>
2020-11-19 12:13:39 +0000

    Added gas resistance

--- raspberry-pi/new-ms430-capture.py
+++ raspberry-pi/new-ms430-capture.py
@@ -33,6 +33,13 @@ clientName = "MS430-Pi"         # Update accordingly
 I2C_bus.write_i2c_block_data(i2c_7bit_address, PARTICLE_SENSOR_SELECT_REG, [PARTICLE_SENSOR])
 I2C_bus.write_i2c_block_data(i2c_7bit_address, CYCLE_TIME_PERIOD_REG, [cycle_period])
 
+# Unicode symbol strings
+CELSIUS_SYMBOL = "\u00B0C"
+FAHRENHEIT_SYMBOL = "\u00B0F"
+SDS011_CONC_SYMBOL = "\u00B5g/m\u00B3" # micrograms per cubic meter
+SUBSCRIPT_2 = "\u2082"
+OHM_SYMBOL = "\u03A9"
+
 #########################################################
 
 print("Entering cycle mode and waiting for data. Press ctrl-c to exit.")
@@ -64,7 +71,7 @@ while (True):
   client.publish("sensors", "temperature,room=office-ms430 value=" + "{:.1f}".format(air_data['T']))
   print("Humidity = {:.1f} %".format(air_data['H_pc']))
   client.publish("sensors", "humidity,room=office-ms430 value=" + "{:.1f}".format(air_data['H_pc']))
-  print("Pressure = " +str(air_data['P_Pa']/100) + " hPa")
+  print("Pressure = " + str(air_data['P_Pa']/100) + " hPa")
   client.publish("sensors", "pressure,room=office-ms430 value=" + str(air_data['P_Pa']/100))
   print("Lux = {:.2f} lux".format(light_data['illum_lux']))
   client.publish("sensors", "lux,room=office-ms430 value=" + "{:.2f}".format(light_data['illum_lux']))
@@ -72,8 +79,10 @@ while (True):
   client.publish("sensors", "airquality,room=office-ms430 value=" + "{:.1f}".format(air_quality_data['AQI']))
   print("Breath VOC = {:.2f} ppm".format(air_quality_data['bVOC']))
   client.publish("sensors", "bvoc,room=office-ms430 value=" + "{:.2f}".format(air_quality_data['bVOC']))
-  print("Estimated CO2 = {:.1f} ppm".format(air_quality_data['CO2e']))
+  print("Estimated CO" + SUBSCRIPT_2 + " = {:.1f} ppm".format(air_quality_data['CO2e']))
   client.publish("sensors", "co2,room=office-ms430 value=" + "{:.1f}".format(air_quality_data['CO2e']))
+  print("Gas sensor resistance = " + str(air_data['G_ohm']) + " " + OHM_SYMBOL)
+  client.publish("sensors", "gas-resistance,room=office-ms430 value=" + str(air_data['G_ohm']))
   print("Peak amplitude = {:.2f} mPa".format(sound_data['peak_amp_mPa']))
   client.publish("sensors", "sound-peak-amp,room=office-ms430 value=" + "{:.2f}".format(sound_data['peak_amp_mPa']))
   print("A-weighted sound pressure = {:.1f} dBA".format(sound_data['SPL_dBA']))
