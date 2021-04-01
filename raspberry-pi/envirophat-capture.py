5e06acfe4c1932db69ea7b64269b78d6b1d662e4
Wesley Archer <wesleyarcher@gmail.com>
2021-04-01 17:16:29 +0100

    Updated to use MQTT reconnection

--- raspberry-pi/envirophat-capture.py
+++ raspberry-pi/envirophat-capture.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# This script will pull data from the BME280 sensor and send it to a MQTT broker.
+# This script will pull data from the Enviro pHAT sensor and send it to a MQTT broker.
 
 import time
 import datetime
@@ -12,24 +12,51 @@ import paho.mqtt.client as mqtt
 # MQTT details
 brokerAddress = "192.168.1.24"  # Update accordingly
 clientName = "OfficePi"         # Update accordingly
+room = "office-enviro"          # Update accordingly
+zone = "downstairs"             # Update accordingly
 
 # Define the time between sending data to MQTT broker - default is 60 seconds
 period = 60
 
-def run():
-  while True:
+# For MQTT connections
+def on_disconnect(client, userdata, rc):
+    if rc!=0:
+      print("MQTT disconnected. Will auto-reconnect...")
+      sys.stdout.flush()
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.connected_flag = True
+        print("Connected to MQTT broker as {}.".format(clientName))
+        sys.stdout.flush()
+    else:
+        print("Connection error! Result code: {}".format(rc))
+        sys.stdout.flush()
+
+mqtt.Client.connected_flag = False
+client = mqtt.Client(clientName)
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
+client.loop_start()
+client.connect(brokerAddress)
+while not client.connected_flag:
+    print("Connecting...")
+    time.sleep(1)
+
+while True:
+  try:
+    # Get the time
+    now = datetime.datetime.now()
     # Get the first reading from the Enviro pHAT
     temperature,pressure,lux = weather.temperature() -5.8, weather.pressure()/100, light.light()
     if temperature is not None and pressure is not None and lux is not None:
         try:
             # Send data to MQTT
             now = datetime.datetime.now()
-            client = mqtt.Client(clientName)
-            client.connect(brokerAddress)
-            client.publish("sensors", "temperature,room=office-enviro,floor=downstairs value=" +str(temperature))
-            client.publish("sensors", "pressure,room=office-enviro,floor=downstairs value=" +str(pressure))
-            client.publish("sensors", "lux,room=office-enviro,floor=downstairs value=" +str(lux))
-            print("Data sent at: " +(now.strftime("%H:%M:%S on %d/%m/%Y")))
+            client.publish("sensors", "temperature,room=" + str(room) + ",floor=" + str(zone) + " value=" + str(temperature))
+            client.publish("sensors", "pressure,room=" + str(room) + ",floor=" + str(zone) + " value=" + str(pressure))
+            client.publish("sensors", "lux,room=" + str(room) + ",floor=" + str(zone) + " value=" + str(lux))
+            print("Data sent to MQTT broker " + str(brokerAddress) + " at " + (now.strftime("%H:%M:%S on %d/%m/%Y")))
             sys.stdout.flush()
         except Exception:
           # Process exception here
@@ -40,4 +67,6 @@ def run():
     # Sleep some time
     time.sleep(period)
 
-run()
+  except (KeyboardInterrupt, SystemExit):
+    sys.exit("Goodbye!")
+    pass
