991335c1433b28dcfbbcc2b1b94628465d1c7bdc
Wesley Archer <wesleyarcher@gmail.com>
2021-04-06 12:21:05 +0100

    Updated BME280 scripts

--- arduino/esp8266-bme280-oled-wifimanager-mqtt.ino
+++ arduino/esp8266-bme280-oled-wifimanager-mqtt.ino
@@ -1,10 +1,14 @@
 #include <ESP8266WiFi.h> // To connect to WiFi
 #include <Wire.h>  // Only needed for Arduino 1.6.5 and earlier
 #include <Adafruit_BME280.h> // BME280 sensor
+#include "SSD1306Wire.h" // legacy include: `#include "SSD1306.h"`
 #include <PubSubClient.h>
 #include <WiFiManager.h>
 #include <Ticker.h>
 Ticker ticker;
+
+// Initialize the OLED display using Wire library
+SSD1306Wire  display(0x3c, D2, D1);  //D2=SDK  D1=SCK  As per labeling on NodeMCU
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 13 // ESP32 DOES NOT DEFINE LED_BUILTIN
@@ -69,6 +73,9 @@ void setup() {
   client.setServer(mqtt_server, 1883);
   Serial.println("Connected to MQTT server: " + String(mqtt_server));
   Serial.println("");
+  Serial.println("Initializing OLED display...");
+  display.init();
+  display.flipScreenVertically();
   Serial.println("Initialising BME280 sensor...");
   bool status;
   status = bme.begin(0x76);  
@@ -96,10 +103,47 @@ void reconnect() {
   }
 }
 
-void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
+void displayTemperature() {
+  //float temperature = bme.readTemperature() - 2.193; // Calibrated value for sensor if housed in a case
+  float temperature = bme.readTemperature();
+  Serial.print("Temperature: ");
+  Serial.print(temperature, 4); Serial.println("°C.");
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Temperature:");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 25, String(temperature, 2) + "°C");
+  display.display();
+}
+
+void displayHumidity() {
+  float humidity = bme.readHumidity();
+  Serial.print("Humidity: ");
+  Serial.print(humidity, 2); Serial.println("% RH.");
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Humidity:");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 25, String(humidity, 2) + "% RH");
+  display.display();
+}
+
+void displayPressure() {
+  float pressure = bme.readPressure() / 100.0F;
+  Serial.print("Pressure: ");
+  Serial.print(pressure, 0); Serial.println("hPa.");
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(64, 10, "Pressure:");
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(64, 25, String(pressure, 2) + "hPa");
+  display.display();
+}
+
+void publishData() {
   //float temperature = bme.readTemperature() - 2.193; // Calibrated value for sensor if housed in a case
   float temperature = bme.readTemperature();
   String v1 = ("temperature,room=" + String(room) + ",floor=" + String(level) + " value=" + String(temperature));
@@ -107,15 +151,22 @@ void loop() {
   String v2 = ("humidity,room=" + String(room) + ",floor=" + String(level) + " value=" + String(humidity));
   float pressure = bme.readPressure() / 100.0F;
   String v3 = ("pressure,room=" + String(room) + ",floor=" + String(level) + " value=" + String(pressure));
-  Serial.print("Temperature: ");
-  Serial.print(temperature, 4); Serial.println("°C.");
-  Serial.print("Humidity: ");
-  Serial.print(humidity, 2); Serial.println("% RH.");
-  Serial.print("Pressure: ");
-  Serial.print(pressure, 0); Serial.println("hPa.");
   client.publish(channel, v1.c_str(), true);
   client.publish(channel, v2.c_str(), true);
   client.publish(channel, v3.c_str(), true);
   Serial.println("Temperature, humidity and pressure data sent to MQTT server!");
-  delay(60000);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  publishData();
+  delay(1000);
+  displayTemperature();
+  delay(20000);
+  displayHumidity();
+  delay(20000);
+  displayPressure();
+  delay(19000);
 }
